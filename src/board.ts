44cf1fd7f166ff7f72fc076fede6f66fa876b00e
Gigz <gigzpumpking@gmail.com>
2023-11-08 19:31:11 -0800

    Section 6, finished Flyweight Pattern of board.ts implementation

---
+++ src/board.ts
@@ -0,0 +1,68 @@
+import leaflet from "leaflet";
+
+interface Cell {
+    readonly i: number;
+    readonly j: number;
+}
+
+export class Board {
+
+    readonly tileWidth: number;
+    readonly tileVisibilityRadius: number;
+
+    private readonly knownCells: Map<string, Cell>;
+
+    constructor(tileWidth: number, tileVisibilityRadius: number) {
+        this.tileWidth = tileWidth;
+        this.tileVisibilityRadius = tileVisibilityRadius;
+        this.knownCells = new Map();
+    }
+
+    private getCanonicalCell(cell: Cell): Cell {
+        const { i, j } = cell;
+        const key = [i, j].toString();
+        const foundCell = this.knownCells.get(key);
+        if (foundCell) {
+            return foundCell;
+        }
+        const canonicalCell = { i, j };
+        this.knownCells.set(key, canonicalCell);
+        return canonicalCell;
+    }
+
+    getCellForPoint(point: leaflet.LatLng): Cell {
+        return this.getCanonicalCell({
+            i: Math.floor(point.lat / this.tileWidth),
+            j: Math.floor(point.lng / this.tileWidth),
+        });
+    }
+
+    getCellBounds(cell: Cell): leaflet.LatLngBounds {
+        const { i, j } = cell;
+        return leaflet.latLngBounds([
+            [i * this.tileWidth, j * this.tileWidth],
+            [(i + 1) * this.tileWidth, (j + 1) * this.tileWidth],
+        ]);
+    }
+
+    getCellsNearPoint(point: leaflet.LatLng): Cell[] {
+        const resultCells: Cell[] = [];
+        const originCell = this.getCellForPoint(point);
+
+        for (let i = -this.tileVisibilityRadius; i <= this.tileVisibilityRadius; i++) {
+            for (let j = -this.tileVisibilityRadius; j <= this.tileVisibilityRadius; j++) {
+                const cell = this.getCanonicalCell({ i: originCell.i + i, j: originCell.j + j });
+                const cellBounds = this.getCellBounds(cell);
+                if (point.distanceTo(cellBounds.getCenter()) <= this.tileVisibilityRadius) {
+                    resultCells.push(cell);
+                }
+            }
+        }
+
+        return resultCells;
+    }
+
+    returnCellCoordinates(cell: Cell): string {
+        return `(${cell.i}, ${cell.j})`;
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,11 @@ import "./style.css";
 import leaflet from "leaflet";
 import luck from "./luck";
 import "./leafletWorkaround";
-
+import { Board } from "./board";
 
 const MERRILL_CLASSROOM = leaflet.latLng({
-    lat: 36.9995,
-    lng: - 122.0533
+  lat: 36.9995,
+  lng: -122.0533,
 });
 
 const GAMEPLAY_ZOOM_LEVEL = 19;
@@ -15,21 +15,26 @@ const TILE_DEGREES = 1e-4;
 const NEIGHBORHOOD_SIZE = 8;
 const PIT_SPAWN_PROBABILITY = 0.1;
 
+const world: Board = new Board(TILE_DEGREES, NEIGHBORHOOD_SIZE);
+
 const mapContainer = document.querySelector<HTMLElement>("#map")!;
 
 const map = leaflet.map(mapContainer, {
-    center: MERRILL_CLASSROOM,
-    zoom: GAMEPLAY_ZOOM_LEVEL,
-    minZoom: GAMEPLAY_ZOOM_LEVEL,
-    maxZoom: GAMEPLAY_ZOOM_LEVEL,
-    zoomControl: false,
-    scrollWheelZoom: false
+  center: MERRILL_CLASSROOM,
+  zoom: GAMEPLAY_ZOOM_LEVEL,
+  minZoom: GAMEPLAY_ZOOM_LEVEL,
+  maxZoom: GAMEPLAY_ZOOM_LEVEL,
+  zoomControl: false,
+  scrollWheelZoom: false,
 });
 
-leaflet.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+leaflet
+  .tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     maxZoom: 19,
-    attribution: "&copy; <a href=\"http://www.openstreetmap.org/copyright\">OpenStreetMap</a>"
-}).addTo(map);
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  })
+  .addTo(map);
 
 const playerMarker = leaflet.marker(MERRILL_CLASSROOM);
 playerMarker.bindTooltip("That's you!");
@@ -37,10 +42,12 @@ playerMarker.addTo(map);
 
 const sensorButton = document.querySelector("#sensor")!;
 sensorButton.addEventListener("click", () => {
-    navigator.geolocation.watchPosition((position) => {
-        playerMarker.setLatLng(leaflet.latLng(position.coords.latitude, position.coords.longitude));
-        map.setView(playerMarker.getLatLng());
-    });
+  navigator.geolocation.watchPosition((position) => {
+    playerMarker.setLatLng(
+      leaflet.latLng(position.coords.latitude, position.coords.longitude)
+    );
+    map.setView(playerMarker.getLatLng());
+  });
 });
 
 let points = 0;
@@ -48,46 +55,50 @@ const statusPanel = document.querySelector<HTMLDivElement>("#statusPanel")!;
 statusPanel.innerHTML = "No points yet...";
 
 function makePit(i: number, j: number) {
-    const bounds = leaflet.latLngBounds([
-        [MERRILL_CLASSROOM.lat + i * TILE_DEGREES,
-        MERRILL_CLASSROOM.lng + j * TILE_DEGREES],
-        [MERRILL_CLASSROOM.lat + (i + 1) * TILE_DEGREES,
-        MERRILL_CLASSROOM.lng + (j + 1) * TILE_DEGREES],
-    ]);
+  const point = leaflet.latLng({
+    lat: MERRILL_CLASSROOM.lat + i * TILE_DEGREES,
+    lng: MERRILL_CLASSROOM.lng + j * TILE_DEGREES,
+  });
+  const cell = world.getCellForPoint(point);
 
-    const pit = leaflet.rectangle(bounds) as leaflet.Layer;
+  const cellBounds = world.getCellBounds(cell);
 
-    pit.bindPopup(() => {
-        let value = Math.floor(luck([i, j, "initialValue"].toString()) * 100);
-        const container = document.createElement("div");
-        container.innerHTML = `
-                <div>There is a pit here at "${i},${j}". It has value <span id="value">${value}</span>.</div>
+  const pit = leaflet.rectangle(cellBounds) as leaflet.Layer;
+
+  const cellCoordinates: string = world.returnCellCoordinates(cell);
+
+  pit.bindPopup(() => {
+    let value = Math.floor(luck([i, j, "initialValue"].toString()) * 100);
+    const container = document.createElement("div");
+    container.innerHTML = `
+                <div>There is a pit here at "${cellCoordinates}". It has value <span id="value">${value}</span>.</div>
                 <button id="poke">poke</button>
-                <button id="store">store</button>`
-            ;
-        const poke = container.querySelector<HTMLButtonElement>("#poke")!;
-        poke.addEventListener("click", () => {
-            value--;
-            container.querySelector<HTMLSpanElement>("#value")!.innerHTML = value.toString();
-            points++;
-            statusPanel.innerHTML = `${points} points accumulated`;
-        });
-        const store = container.querySelector<HTMLButtonElement>("#store")!;
-        store.addEventListener("click", () => {
-            points--;
-            statusPanel.innerHTML = `${points} points accumulated`;
-            value++;
-            container.querySelector<HTMLSpanElement>("#value")!.innerHTML = value.toString();
-        });
-        return container;
+                <button id="store">store</button>`;
+    const poke = container.querySelector<HTMLButtonElement>("#poke")!;
+    poke.addEventListener("click", () => {
+      value--;
+      container.querySelector<HTMLSpanElement>("#value")!.innerHTML =
+        value.toString();
+      points++;
+      statusPanel.innerHTML = `${points} points accumulated`;
     });
-    pit.addTo(map);
+    const store = container.querySelector<HTMLButtonElement>("#store")!;
+    store.addEventListener("click", () => {
+      points--;
+      statusPanel.innerHTML = `${points} points accumulated`;
+      value++;
+      container.querySelector<HTMLSpanElement>("#value")!.innerHTML =
+        value.toString();
+    });
+    return container;
+  });
+  pit.addTo(map);
 }
 
 for (let i = -NEIGHBORHOOD_SIZE; i < NEIGHBORHOOD_SIZE; i++) {
-    for (let j = - NEIGHBORHOOD_SIZE; j < NEIGHBORHOOD_SIZE; j++) {
-        if (luck([i, j].toString()) < PIT_SPAWN_PROBABILITY) {
-            makePit(i, j);
-        }
+  for (let j = -NEIGHBORHOOD_SIZE; j < NEIGHBORHOOD_SIZE; j++) {
+    if (luck([i, j].toString()) < PIT_SPAWN_PROBABILITY) {
+      makePit(i, j);
     }
+  }
 }
